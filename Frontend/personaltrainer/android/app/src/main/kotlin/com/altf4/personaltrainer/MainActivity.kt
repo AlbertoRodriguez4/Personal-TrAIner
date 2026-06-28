b548cdbcad3d34e579ae32e0963c44aff421f38d
Alberto Rodriguez Penalva <127990307+AlbertoRodriguez4@users.noreply.github.com>
2026-06-28 13:45:36 +0200

    fix: app now successfully fetches multiple data records from Health Connect

--- Frontend/personaltrainer/android/app/src/main/kotlin/com/altf4/personaltrainer/MainActivity.kt
+++ Frontend/personaltrainer/android/app/src/main/kotlin/com/altf4/personaltrainer/MainActivity.kt
@@ -4,11 +4,11 @@ import android.content.BroadcastReceiver
 import android.content.Context
 import android.content.Intent
 import android.content.IntentFilter
-import io.flutter.embedding.android.FlutterActivity
+import io.flutter.embedding.android.FlutterFragmentActivity
 import io.flutter.embedding.engine.FlutterEngine
 import io.flutter.plugin.common.EventChannel
 
-class MainActivity : FlutterActivity() {
+class MainActivity : FlutterFragmentActivity() {
 
     private val CHANNEL = "traininghub.dev/hr_stream"
     private val GB_ACTION = "nodomain.freeyourgadget.gadgetbridge.device.action.HEART_RATE_UPDATED"
